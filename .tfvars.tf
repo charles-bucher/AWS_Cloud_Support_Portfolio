d6340ad310a7974ca8de3c9202049e01824a2a22
Charles Bucher <your-email@example.com>
2026-01-08 21:20:02 -0500

    Clean up scripts, add missing screenshots, update README

---
+++ .tfvars.tf
@@ -0,0 +1,105 @@
+Skip to content
+Navigation Menu
+charles-bucher
+
+Overview
+Repositories
+7
+Projects
+Packages
+Stars
+
+Charles Bucher
+charles-bucher · he/him
+Entry‑level AWS Cloud Support Engineer • EC2, S3, Lambda • Python, Bash, PowerShell • CloudOps automation
+ 7 followers · 5 following
+Florida
+quietopscb@gmail.com
+https://charles-bucher.github.io/
+in/charles-bucher-cloud
+https://profile.indeed.com/p/charlesb-x0xr5fx
+https://github.com/charles-bucher/charles-bucher
+Find a repository…
+AWS_Cloud_Support_Sim Public
+Hands-on AWS Cloud Support labs: EC2, S3, Lambda, IAM, GuardDuty, CloudWatch, incident response, automation, and CloudOps troubleshooting
+python
+bash
+aws
+portfolio
+devops
+lambda
+monitoring
+ Python Other Updated 10 minutes ago
+AWS_Error_Driven_Troubleshooting_Lab Public
+Intentionally broken AWS scenarios for hands-on troubleshooting using real cloud support workflows. Focused on logs, metrics, root cause analysis, remediation, and prevention — not tutorials or gui…
+python
+linux
+bash
+aws
+portfolio
+automation
+monitoring
+ Python Other Updated yesterday
+charles-bucher Public
+“AWS CloudOps & IT Support | Hands-On Projects with EC2, S3, VPC, Python, Terraform & CloudFormation – Skills & Learning Journey
+python
+aws
+portfolio
+devops
+lambda
+automation
+cloudformation
+ Python Other Updated 2 days ago
+charles-bucher.github.io Public
+AWS Cloud Support & DevOps portfolio showcasing hands-on labs, VPC/EC2/Terraform projects, Lambda automation, and cloud troubleshooting expertise.
+python
+linux
+bash
+aws
+portfolio
+devops
+automation
+ HTML Other Updated 2 days ago
+CloudOpsLab Public
+CloudOpsLab: Hands-on AWS and cloud support scripts showcasing troubleshooting, automation, monitoring, and self-healing. Demonstrates practical CloudOps skills, diagnostics, and cloud problem-solv…
+python
+linux
+bash
+aws
+portfolio
+devops
+lambda
+ Python  1  1 MIT License Updated 2 days ago
+Security-Compliance-Guardrail-Lab Public
+🔒 Hands-on AWS lab simulating misconfigurations and enforcing automated guardrails. Demonstrates IAM, S3, CloudWatch, Lambda, CloudTrail, incident response, and preventative CloudOps skills—portfol…
+python
+aws
+security
+portfolio
+devops
+lambda
+monitoring
+Other Updated 2 days ago
+AWS-Cost-Optimization-Tool- Public
+Python/PowerShell tool to analyze and optimize AWS costs. Automates cost reporting, detects underutilized EC2, S3, Lambda, and other resources, and delivers actionable CloudOps recommendations—idea…
+python
+bash
+aws
+portfolio
+automation
+monitoring
+ec2
+ Python MIT License Updated 2 days ago
+Footer
+© 2026 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Community
+Docs
+Contact
+Manage cookies
+Do not share my personal information
+ 
